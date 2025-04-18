aba26c890bc96128810f78c686b6b77b7247b059
Gleybe Hebson Salazar Cavalcanti <gleybe.cavalcanti@vectracs.com.br>
2025-04-18 03:01:05 -0300

    CRUD de usuários finalizado com uma parte do refresh token renovado

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -23,3 +23,58 @@ exports.create = async (req, res) => {
     return errorResponse(res, err.message || 'Erro interno ao criar usuário', 400);
   }
 };
+
+exports.delete = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = req.user.id;
+
+    // Impede autoexclusão
+    if (id === currentUserId) {
+      return errorResponse(res, 'Você não pode excluir a si mesmo.', 403);
+    }
+
+    const deleted = await UserService.deleteUser(id);
+
+    if (!deleted) {
+      return errorResponse(res, 'Usuário não encontrado ou já inativo.', 404);
+    }
+
+    return successResponse(res, 'Usuário excluído com sucesso.', 204);
+  } catch (err) {
+    console.error('Erro ao excluir usuário:', err);
+    return errorResponse(res, 'Erro ao excluir usuário.', 500, err);
+  }
+};
+
+exports.getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await UserService.getUserById(id);
+
+    if (!user) {
+      return errorResponse(res, 'Usuário não encontrado ou inativo.', 404);
+    }
+
+    return successResponse(res, 'Usuário encontrado.', 200, user);
+  } catch (err) {
+    console.error('Erro ao buscar usuário por ID:', err);
+    return errorResponse(res, 'Erro ao buscar usuário.', 500, err);
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await UserService.updateUser(id, req.body);
+
+    if (!user) {
+      return errorResponse(res, 'Usuário não encontrado ou inativo.', 404);
+    }
+
+    return successResponse(res, 'Usuário atualizado com sucesso.', 200, user);
+  } catch (err) {
+    return errorResponse(res, err?.message ?? 'Erro ao atualizar usuário.', 500, {err});
+  }
+};
